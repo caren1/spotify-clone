21937c73631e511a78125027b4002570c7d55a5f
caren1 <czarnocki.w@wp.pl>
2020-08-31 19:43:40 +0200

    initialized Login component + styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="app">
-
+      <Login />
      </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Login.css'
+
+// Draft for the application login page
+
+// 1 - Spotify Logo
+// 2 - Login with spotify button
+
+function Login() {
+    return (
+        <div className="login">
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
+            <a href="">Login with spotify</a>
+        </div>
+    )
+}
+
+export default Login
