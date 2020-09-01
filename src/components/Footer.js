0d628265ca78514ee4da48b58d17f7a6a0433436
caren1 <czarnocki.w@wp.pl>
2020-09-01 17:04:08 +0200

    finished the Footer component + styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,14 +17,18 @@ function Footer() {
 
             {/* Album and song details  */}
             <div className="footer__left">
-                <p>left</p>
+                <img src="" alt="" className="footer__albumLogo"/>
+                <div class="footer__songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
             </div>
 
             {/* Player controls */}
             <div className="footer__center">
                 <ShuffleIcon className="footer__green"/>
                 <SkipPreviousIcon className="footer__icon"/>
-                <PlayCircleOutlineIcon fontSie="large" className="footer__icon"/>
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon"/>
                 <SkipNextIcon className="footer__icon"/>
                 <RepeatIcon className="footer__green"/>
             </div>
